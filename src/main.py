0bc31ca11c4fb18c73b655ceec0380ed0a7c025f
Julien Fleury <julienfl94@gmail.com>
2025-04-14 22:48:40 +0200

    xpath with parent+string

--- src/main.py
+++ src/main.py
@@ -1,23 +1,39 @@
 from lxml import html
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
 import lxml.html
 import urllib.request
+import sys
 
+#Local file
+# with open('index.html', 'r') as f:
+#     page = f.read()
 
-def extraire_content(url, xpath):
-    ## Ouvrir l'URL
-    site = urllib.request.urlopen(url).read()
-    ## Parser le contenu HTML
-    lxml_site = lxml.html.fromstring(site)
-    ## Extraire le contenu avec XPath
-    description = lxml_site.xpath(xpath)[0]
-    ## Convertir le contenu en texte brut
-    text = description.text_content()
+def find_string(url):
+    if(len(sys.argv) != 2):
+        print ("String to search is needed")
+        return
     
-    print(text)
+    stringToFind = sys.argv[1]
+    
+    response = urllib.request.urlopen(url)
+    html_content = response.read()
+    parsed_soup = soup(html_content, 'html.parser')
+    
+    
+    a_string = parsed_soup.find(string=stringToFind)
+    
+    if not a_string:
+        print("String not found")
+        return
+    else:
+        res = a_string.find_parent()
+        print(res)
+        print(res.name)
+        print(res.attrs)
 
+    xpath='xpath=//' + res.name + '[@text()="' + stringToFind + '"]'
+    print("Xpath: " + xpath)
+    
 if __name__ == '__main__':
     url = 'https://sulphers.github.io'
-    xpath = "//div[@class='hero-content']"
-    res = extraire_content(url, xpath)
-
+    find_string(url)
